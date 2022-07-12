8f23075363e93663bba977a51213f97163e9a4a1
Emma <emma@ubuntu.Home>
2022-07-12 14:50:02 +0100

    set up Search component

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import "../styles/search.css";
+
+const Search = () => {
+  return (
+    <>
+      <input className="search-input" type="text" />
+    </>
+  );
+};
+export default Search;
